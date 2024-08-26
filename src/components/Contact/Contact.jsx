fd68eed6b817ee4936b025dc1c36cd046b6deb19
[Ritik] <[ritikkumar8077008067@gmail.com]>
2024-08-26 13:50:50 +0530

    hey

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -43,10 +43,10 @@ Phone: ${formData.phone}`;
         <h1 className='flex justify-center text-3xl md:text-5xl'>Contact Form</h1>
         <h2 className='mt-10 md:mt-20'>
           Hello Sir! , Please Enter Your Name Here <span><input className='border-b-2 border-black w-full md:w-auto' type="text" name="name" value={formData.name} onChange={handleChange} required /></span>  This is Contact Form   <br className='md:hidden'/><br className='md:hidden'/>
-          Please Enter Your Email Here <input className='border-b-2 border-black w-full md:w-auto' type="email" name="email" value={formData.email} onChange={handleChange} required /> , For a Easy Contact :  <br className='md:hidden'/><br className='md:hidden'/>
+          Please Enter Your Email Here <input className='border-b-2 border-black w-full md:w-auto' type="email" name="email" value={formData.email} onChange={handleChange} required />  For a Easy Contact :  <br className='md:hidden'/><br className='md:hidden'/>
           Can You Write Down Your Mobile Number <input className='border-b-2 border-black w-full md:w-auto' type="tel" name="phone" value={formData.phone} onChange={handleChange} required /> , <br className='mt-4 md:mt-8' /> <br />
         </h2>
-        <h2 className='mt-6 md:mt-10'>Thanks For Reaching me out ,  we will <span className='text-blue-700'>connect</span> soon .</h2>
+        <h2 className='mt-6 md:mt-10'>Thanks For Reaching me  ,  we will <span className='text-blue-700'>connect</span> soon .</h2>
         <div className='mt-6 md:mt-10 flex justify-center'>
           <button type="submit" className='bg-black text-xl text-white font-bold py-2  px-4 rounded'>
             Submit
