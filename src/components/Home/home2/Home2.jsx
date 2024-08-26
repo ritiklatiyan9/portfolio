583c3f76342e5942e1d42e24a3177c0c8ea5ceb9
[Ritik] <[ritikkumar8077008067@gmail.com]>
2024-08-26 12:54:16 +0530

    fixed

--- src/components/Home/home2/Home2.jsx
+++ src/components/Home/home2/Home2.jsx
@@ -38,13 +38,13 @@ function Home2() {
   }, [inView, controls, textControls]);
 
   return (
-    <div className="relative flex flex-col md:flex-row items-center p-4 md:p-20 overflow-hidden">
+    <div className="relative flex flex-col md:flex-row items-center  md:p-8 lg:p-20 overflow-hidden space-y-6 md:space-y-0">
       <motion.div
         animate={controls}
-        className="flex-shrink-0 rounded  overflow-hidden w-full md:w-1/2"
+        className="flex-shrink-0 rounded overflow-hidden w-full md:w-1/2"
       >
         <img
-          className="w-full h-auto object-cover"
+          className="w-full ml-4 h-auto object-cover"
           src={techy}
           alt="Techy"
         />
@@ -53,13 +53,13 @@ function Home2() {
       <motion.div
         ref={ref}
         animate={textControls}
-        className="w-full md:w-1/2 flex flex-col p-4 md:p-8 mt-4 md:mt-0"
+        className="w-full md:w-1/2 flex flex-col p-2 text-center md:text-left md:p-8"
       >
-        <span className="text-5xl shadow-xl font-bold border-b-2 flex justify-center border-zinc-800 px-2 py-4  rounded-2xl text-zinc-900 leading-none">
-          LATEST TECH . 
+        <span className="text-3xl md:text-4xl lg:text-5xl flex justify-center font-bold border-b-2 border-zinc-800 px-2 py-4 rounded-2xl text-zinc-900">
+          LATEST TECH.
         </span>
-        <h2 className="text-sm flex justify-center p-4 md:text-xl font-semibold leading-tight capitalize mt-2">
-        Blockchain technology is expanding beyond cryptocurrencies, finding applications in secure digital transactions and supply chain management. Additionally, sustainable tech, including green energy innovations and electric vehicles, is gaining momentum as the world focuses on environmental responsibility.
+        <h2 className="text-base md:text-lg lg:text-xl p-2 font-semibold leading-tight mt-2 text-zinc-700">
+          Blockchain technology is expanding beyond cryptocurrencies, finding applications in secure digital transactions and supply chain management. Additionally, sustainable tech, including green energy innovations and electric vehicles, is gaining momentum as the world focuses on environmental responsibility.
         </h2>
       </motion.div>
     </div>
